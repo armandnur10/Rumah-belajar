94ced4ea245dd3dc149c3a04b5e090c5d1a3bfed
AbdiRabbani <abdirabbani59@gmail.com>
2022-10-28 14:22:18 +0700

    form page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -17,3 +17,23 @@ window.addEventListener('scroll', e => {
         navHeader.classList.remove('on-scroll')
     }
 })
+
+const scriptURL = ''
+        const form = document.forms['comment']
+      
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                console.log('Success!', response)
+                document.getElementById("call-text").textContent = "Succes send the mail";
+                document.getElementById("callout").style.right = "20px";
+                form.reset()
+            })
+            .catch(error => {
+                console.log('Success!', response)
+                document.getElementById("call-text").textContent = "Succes send the mail";
+                document.getElementById("callout").style.right = "20px";
+                form.reset()
+            })
+        })
