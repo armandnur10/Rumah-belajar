29f2890e847bd3bf76dc130f62c293c6d4b922ce
AbdiRabbani <abdirabbani59@gmail.com>
2022-10-28 08:50:32 +0700

    make animation navHeader and add styling scrollbar

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,3 +7,13 @@ const textArray = [
 setInterval(() => {
     document.title = textArray[idx++ % textArray.length]
 }, 1500);
+
+const navHeader = document.querySelector('header')
+
+window.addEventListener('scroll', e => {
+    if(window.scrollY > 50) {
+        navHeader.classList.add('on-scroll')
+    }else {
+        navHeader.classList.remove('on-scroll')
+    }
+})
